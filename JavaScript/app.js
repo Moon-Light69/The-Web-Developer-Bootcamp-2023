47a83bb59c691dbbfae321dd00bd2fb988165ed8
Moon-Light69 <danial.ghobadi@yahoo.com>
2023-06-27 21:13:43 +0300

    DanakWeb

---
+++ JavaScript/app.js
@@ -0,0 +1,88 @@
+// let random = math.random();
+// if (random < 0.5) {
+//     console.log("your number is less")
+//     console.log(random);
+// }
+
+
+
+// const age = prompt("please enter your age")
+
+// if (age < 5) {
+//     console.log('it is free for you beacuse you are a baby')
+// } else if (age < 10) {
+//     console.log("you must pay $20")
+// } else {
+//     console.log("do not do this again, okay!??")
+// }
+
+// const password = prompt("please enter your password")
+
+// if (password.length >= 6){
+//     console.log("well done")
+// } else{
+//     console.log("it is a short password")
+// }
+
+// if (password.indexOf(' ') === -1){
+//     console.log("good job")
+// } else{
+//     console.log("it is not good body")
+// }
+
+// const userInput = prompt('enter a wold')
+
+// if (userInput){
+//     console.log("well down!!!")
+// } else{
+//     console.log("heeey enter somethink")
+// }
+
+// let userInput = prompt("enter a password")
+
+// if (userInput.length >= 6 && userInput.indexOf(" ") === -1) {
+//     console.log("well down it is awsome")
+// } else{
+//     console.log("nope it is false")
+// }
+
+// var userInput = prompt("enter a number");
+
+// if (userInput < 0 && userInput > 5) {
+//     console.log("sorry we can not sell this to you!!!");
+// } else if (!(userInput >= 5 && userInput <= 100)) {
+//     console.log("yeeeeeeeeeees it is working");
+// }
+
+// const lang = 4
+
+// switch (lang) {
+//     case 1:
+//         console.log("hello");
+//         break
+
+//     case 2:
+//         console.log("merhaba")
+//         break
+
+//     case 3:
+//         console.log("سلام")
+//         break
+    
+//     default:
+//         console.log("it is nit a lang")
+// }
+
+
+// for (let number = 10; number >= 10; number++){
+//     console.log(number)
+// }
+
+// let userlist = {
+//     userName : "danial",
+//     userLastName : "givi ghobadi",
+// };
+
+for (let i = 0; i <= 10; i++){
+    console.log(i)
+}
